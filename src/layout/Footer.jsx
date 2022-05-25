bc845d61f9575c882737f38ce229441b4be2a8ed
Simone Kahmeyer <jskahmeyer@gmail.com>
2022-05-25 14:28:52 -0700

    Hid footer button linking to portfolio page from portfolio page

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import PageLinks from '../components/PageLinks'
 import arrow from '../assets/images/icons/icon-arrow.svg'
 import footerLogo from '../assets/images/archLogoWhite.svg'
 
-const Footer = () => (
+const Footer = () => {
+    const location = useLocation()
+
+    return (
     <div className="footer">
         <div className="footer-logo-container">
             <Link to="/">
@@ -13,11 +17,13 @@ const Footer = () => (
         <div className="footer-links">
             <PageLinks />
         </div>
+        {location.pathname !== '/portfolio' && 
         <Link to="/portfolio" className="footer-button button">
             <span>See Our Portfolio</span>
             <img className="footer-button-arrow" src={arrow} alt="Arrow to redirect to portfolio" />
         </Link>
+}
     </div>
-)
+)}
 
 export default Footer;
